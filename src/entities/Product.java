bfb04cd17a48dbdf4e2599f6c6336fbaa1be539f
şahin <sahinxyt@gmail.com>
2026-01-04 20:13:58 +0300

    StockManager sınıfı için Unit Testler eklendi.

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -2,38 +2,53 @@ package entities;
 
 import entities.Supplier;
 
-
 public class Product {
-    //ürüne ait özellikler
+    // ürüne ait özellikler
     private String id;
     private String name;
     private double price;
     private int stockQuantity;
-private Supplier supplier;
-    //ürüne ait özelliklerin değerleri,parametreler
-    public Product(String id, String name, double price, int stockQuantity,Supplier supplier) {
+    private Supplier supplier;
+
+    // ürüne ait özelliklerin değerleri,parametreler
+    public Product(String id, String name, double price, int stockQuantity, Supplier supplier) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.stockQuantity = stockQuantity;
-        this.supplier=supplier;
+        this.supplier = supplier;
     }
 
-    //ürün sınıfının get ve set metodlarını yazıyoruz
-    //encapsulation yapıyoruz
-    public String getId() { return id; }
-    public void setName(String name) { this.name = name; }
+    // ürün sınıfının get ve set metodlarını yazıyoruz
+    // encapsulation yapıyoruz
+    public String getId() {
+        return id;
+    }
 
-    public int getStockQuantity() { return stockQuantity; }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    //stok sayısının 0'dan az girilmesini engelliyoruz
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    // stok sayısının 0'dan az girilmesini engelliyoruz
     public void setStockQuantity(int stockQuantity) {
-        if(stockQuantity >= 0) this.stockQuantity = stockQuantity;
+        if (stockQuantity >= 0) this.stockQuantity = stockQuantity;
     }
 
-    public double getPrice() { return price; }
-    public void setPrice(double price) { this.price = price; }
-    public String getName() { return name; }
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
 
     public Supplier getSupplier() {
         return supplier;
